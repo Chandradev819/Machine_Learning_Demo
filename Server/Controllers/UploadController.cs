8af3b76316717aff046a239e08b39beefd084fc6
Chandradev <chandradev819@hotmail.com>
2021-06-17 22:04:40 +0530

    Add project files.

---
+++ Server/Controllers/UploadController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using ML_Demo3.Shared;
+using ML_Demo3_Server;
+using System;
+using System.Threading.Tasks;
+
+namespace ML_Demo3.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly IWebHostEnvironment env;
+
+        public UploadController(IWebHostEnvironment env)
+        {
+            this.env = env;
+        }
+
+        [HttpPost]
+        public async Task<ImageFile> Post([FromBody] ImageFile[] files)
+        {
+            ImageFile obj = new ImageFile();
+            foreach (var file in files)
+            {
+                var buf = Convert.FromBase64String(file.base64data);
+                obj.filePath = (env.ContentRootPath + "\\" + "Images" + "\\" + Guid.NewGuid().ToString("N") + "-" + file.fileName);
+                await System.IO.File.WriteAllBytesAsync(obj.filePath, buf);
+                obj.result = ImageDefectionFromML(obj.filePath);
+            }
+            return obj;
+        }
+
+        public string ImageDefectionFromML(string imageUrl)
+        {
+            var sampleData = new MLModel.ModelInput()
+            {
+                ImageSource = imageUrl,
+            };
+
+            //Load model and predict output
+            var result = MLModel.Predict(sampleData);
+
+            return result.Prediction.ToString();
+        }
+    }
+}
---
+++ Shared/ImageFile.cs
@@ -0,0 +1,11 @@
+﻿namespace ML_Demo3.Shared
+{
+    public class ImageFile
+    {
+        public string base64data { get; set; }
+        public string contentType { get; set; }
+        public string fileName { get; set; }
+        public string filePath { get; set; }
+        public string result { get; set; }
+    }
+}
